974db8ccdc8cfb4d047f0af0b6c1f0bb61e322ae
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-06-20 18:32:06 -0500

    JS mess

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
 const beginGame = document.getElementById("start-button")
 const questionBox =document.getElementById('questions-answers')
-const questionElement=document.getElementById('questions')
-const answerElement=document.getElementById('answers')
-beginGame.addEventListener('click', gameBegins)
+const questionElement=document.getElementById('test-questions')
+const answerElement=document.getElementById('answer-choices')
 
+let shuffledQuestions, currentQuestionIndex
+
+beginGame.addEventListener('click', gameBegins)
 
 // not working below nothing
 function gameBegins() {
  console.log('start game')
  beginGame.classList.add('hide')
+ shuffledQuestions=questions.sort(() => Math.random() - .5)
+ currentQuestionIndex = 0
  questionBox.classList.remove('hide')
-nextQuestion()
+ setNextQuestion()
+}
+
+function setNextQuestion(){
+    nextQuestion(shuffledQuestions[currentQuestionIndex])
 }
 // error line 17 and 13
 function nextQuestion(question) {
-    // questionElement.innerText=question.question
+   questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+     const button  = document.createElement('button') 
+     button.innerText = answer.text
+     button.classList.add('choice')
+     if(answer.correct) {
+        button.dataset.correct = answer.correct
+     }
+     button.addEventListener('click', selectChoice)
+     answerElement.appendChild(button)
+
+    });
 }
 
 function selectChoice() {
-
+  
 } 
+
+// function reset
+
 const questions = [
     {
-        questions: '1 + 1',
-        answer: [
+        question: '1 + 1=?',
+        answers: [
             {text: 3, correct: false},
             {text: 2, correct: true},
             {text: 4, correct: false},
