2646a4f2dae8064dba885fe2058db11a8e53d690
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-06-20 20:45:37 -0500

    JS complicated

--- script.js
+++ script.js
@@ -1,57 +1,77 @@
-const beginGame = document.getElementById("start-button")
-const questionBox =document.getElementById('questions-answers')
-const questionElement=document.getElementById('test-questions')
-const answerElement=document.getElementById('answer-choices')
+var beginGame = document.getElementById("start-button")
+var questionBox =document.getElementById('questions-answers')
+var questionElement=document.getElementById('test-questions')
+var answerElement=document.getElementById('answer-choices')
 
-let shuffledQuestions, currentQuestionIndex
+// let shuffledQuestions, currentQuestionIndex
 
 beginGame.addEventListener('click', gameBegins)
 
 // not working below nothing
 function gameBegins() {
  console.log('start game')
- beginGame.classList.add('hide')
- shuffledQuestions=questions.sort(() => Math.random() - .5)
- currentQuestionIndex = 0
- questionBox.classList.remove('hide')
- setNextQuestion()
+
+      
+
+
+// //  beginGame.classList.add('hide')
+//  shuffledQuestions = questions.sort(() => Math.random() - .5)
+//  currentQuestionIndex = 0
+// //  questionBox.classList.remove('hide')
+//  setNextQuestion()
 }
 
-function setNextQuestion(){
-    nextQuestion(shuffledQuestions[currentQuestionIndex])
-}
+// function setNextQuestion(){
+//     nextQuestion(shuffledQuestions[currentQuestionIndex])
+// }
 // error line 17 and 13
-function nextQuestion(question) {
-   questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-     const button  = document.createElement('button') 
-     button.innerText = answer.text
-     button.classList.add('choice')
-     if(answer.correct) {
-        button.dataset.correct = answer.correct
-     }
-     button.addEventListener('click', selectChoice)
-     answerElement.appendChild(button)
+function nextQuestion() {
 
-    });
+//    questionElement.innerText = question.question
+//     question.answers.forEach(answer => {
+//      const button  = document.createElement('button') 
+//      button.innerText = answer.text
+//      button.classList.add('choice')
+//      if(answer.correct) {
+//         button.dataset.correct = answer.correct
+//      }
+//      button.addEventListener('click', selectChoice)
+//      answerElement.appendChild(button)
+
+ //   });
 }
 
 function selectChoice() {
   
 } 
 
-// function reset
 
-const questions = [
+// questions and answers
+var questions = [ 
     {
-        question: '1 + 1=?',
-        answers: [
-            {text: 3, correct: false},
-            {text: 2, correct: true},
-            {text: 4, correct: false},
-            {text: 1, correct: false}
-        ] 
+    question: "eij iajei ailr jierja iaj; ",
+    answers: ["2", "4", "5'", "8" ],
+    correct: 1
 
+},
+    {
+    question:
+    answers:
+    correct:
+},
+    {
+    question:
+    answers:
+    correct:   
+},
+    {
+    question:
+    answers:
+    correct:
+},
+    {
+    question:
+    answers:
+    correct:    
     }
-
 ]
